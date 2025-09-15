75953a55bc8087f7cb5c767c03ad8407abc86a87
Hosh1Winter <hoshiwinter@proton.me>
2025-09-15 14:59:40 -0600

    json function

--- setup.py
+++ setup.py
@@ -29,15 +29,13 @@ def main():
                 break
             except Exception as e:
                 print("Not an integer, try again")
-
         conf_dictionary = {"jdk8_path":jdk8_path,"jdk17_path":jdk17_path,"jdk21_path":jdk21_path}
-
         with open('config.json', 'w') as conf:
-            conf.write(json)
+            json.dump(conf_dictionary, conf, indent=4)
 
         print("\nDone")
     except Exception as e:
-        print(f"\nFailed:{e}\n")
+        print(f"\nFailed: {e}\n")
     finally:
         input("Press enter to exit: ")
 
